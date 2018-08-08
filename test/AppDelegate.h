b518402cb004a59b9cdb5940e6af5ebcf50d24e1
YUNXUNKEJI <yunxunkeji@YUNXUNKEJIdeMacBook-Pro.local>
2018-08-08 21:44:30 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by yunxunkeji on 2018/8/8.
+//  Copyright © 2018年 SMF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by yunxunkeji on 2018/8/8.
+//  Copyright © 2018年 SMF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
